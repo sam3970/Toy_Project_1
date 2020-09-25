10877b3e8fbe179407d353ff13e556a67376b687
SangKyu Lee <sam3970@gmail.com>
2020-09-26 08:21:09 +0900

    second ver

--- baekjoon_tag.py
+++ baekjoon_tag.py
@@ -31,3 +31,18 @@ for category in categorys:
     categoryNames.append(categoryName)
 
 print(categoryNames)
+
+
+for categoryN in categoryNames:
+    browser.get('https://www.acmicpc.net/problem/tag/' + categoryN)
+    trs = browser.find_elements_by_css_selector('table#problemset tbody tr')
+
+
+for tr in trs:
+        bj_num = tr.find_element_by_css_selector('td.list_problem_id').text
+        title = tr.find_element_by_css_selector('td:nth-child(2)').text
+        solvers = tr.find_element_by_css_selector('td:nth-child(4)').text
+        submitCnt = tr.find_element_by_css_selector('td:nth-child(5)').text
+        solveRate = tr.find_element_by_css_selector('td:nth-child(6)').text
+        print("{0} {1} {2} {3} {4}".format(bj_num, title, solvers, submitCnt, solveRate))
+'''
